d599980ad13867a4823ae180b4ab13d3412f494b
priyanshu655 <patelpriyanshu1209@gmail.com>
2026-07-01 12:53:05 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,7 +10,7 @@ import Billing from './pages/Billing'
 import { Toaster } from "react-hot-toast"
 
 export const ServerUrl = "https://ai-voice-assistance-qnvj.onrender.com"
-export const CLIENT_URL = "http://localhost:5173"
+export const CLIENT_URL = "https://nexusai-hg3u.onrender.com"
 
 function App() {
   const [user, setUser] = useState(null)
